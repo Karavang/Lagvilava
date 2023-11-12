82cc2f554521c55e4a1fc8584484c3910ef5d7a8
Karavang <k4r4v4ng@gmail.com>
2023-11-12 21:05:46 +0100

    +h3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,8 @@ const Hero = () => {
   return (
     <div className="hero-pos">
       <div className="hero">
-        <h1 className="main-title">Етажерки в стилі “ПРОВАНС”</h1>
+        <h1 className="main-title">Етажерки в стилі “ПРОВАНС”</h1>{" "}
+        <h3 className="delivery-h3">Доставка по всій Україні</h3>
         <button className="make-order-button">Замовити послугу</button>
       </div>
     </div>
